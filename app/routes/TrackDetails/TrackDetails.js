92aa59249642e19041d2c7ae8368ef061efe05c1
Alexandre Palo <a-palo@laposte.net>
2017-05-21 15:37:38 +0200

    Styles for TrackDetails

--- app/routes/TrackDetails/TrackDetails.js
+++ app/routes/TrackDetails/TrackDetails.js
@@ -3,7 +3,9 @@ import { View, Text } from 'react-native'
 import { connect } from 'react-redux'
 import { graphql, gql } from 'react-apollo'
 import moment from 'moment'
+import { MaterialIcons } from '@expo/vector-icons'
 import { Card, Spinner } from '../../components'
+import { styles } from './styles'
 
 class TrackDetailsUnlinked extends Component {
   render() {
@@ -18,17 +20,31 @@ class TrackDetailsUnlinked extends Component {
     return (
       <View>
         <Card>
-          <Text>{name}</Text>
-          <View>
-            <Text>{distance}</Text>
-            <Text>{place}</Text>
+          <View style={styles.containerInfoStl}>
+            <Text style={styles.titleStl}>{name}</Text>
+            <View style={styles.disPlaceContainerStl}>
+              <View style={styles.infoWrapperStl}>
+                <MaterialIcons name="all-inclusive" size={20} color="#000" style={styles.infoIconStl} />
+                <Text style={styles.subTitleStl}>{distance} km</Text>
+              </View>
+              <View style={styles.infoWrapperStl}>
+                <MaterialIcons name="place" size={20} color="#000" style={styles.infoIconStl} />
+                <Text style={styles.subTitleStl}>{place}</Text>
+              </View>
+            </View>
+            <View style={styles.stepContainerStl}>
+              <View style={styles.infoWrapperStl}>
+                <MaterialIcons name="trending-up" size={20} color="#000" style={styles.infoIconStl} />
+                <Text style={styles.subTitleStl}>{stepP} m</Text>
+              </View>
+              <View style={styles.infoWrapperStl}>
+                <MaterialIcons name="trending-down" size={20} color="#000" style={styles.infoIconStl} />
+                <Text style={styles.subTitleStl}>{stepN} m</Text>
+              </View>
+            </View>
+            <Text style={styles.createdAtStl}>Créé le {moment(createdAt).format('DD/MM/YY')}</Text>
+            <Text style={styles.descriptionStl}>{description}</Text>
           </View>
-          <View>
-            <Text>{stepP}</Text>
-            <Text>{stepN}</Text>
-          </View>
-          <Text>{moment(createdAt).format('DD/MM/YY')}</Text>
-          <Text>{description}</Text>
         </Card>
         <Card>
           <Text>Carte</Text>
---
+++ app/routes/TrackDetails/styles.js
@@ -0,0 +1,50 @@
+const styles = {
+  containerInfoStl: {
+    flexDirection: 'column'
+  },
+  titleStl: {
+    alignSelf: 'center',
+    fontSize: 20,
+    color: '#000',
+    opacity: 0.87
+  },
+  disPlaceContainerStl: {
+    marginTop: 5,
+    flexDirection: 'row',
+    justifyContent: 'space-around'
+  },
+  stepContainerStl: {
+    marginTop: 5,
+    flexDirection: 'row',
+    justifyContent: 'space-around'
+  },
+  subTitleStl: {
+    color: '#000',
+    opacity: 0.54
+  },
+  infoIconStl: {
+    color: '#000',
+    opacity: 0.54,
+    marginRight: 10
+  },
+  infoWrapperStl: {
+    flexDirection: 'row',
+    justifyContent: 'center'
+  },
+  createdAtStl: {
+    color: '#000',
+    opacity: 0.54,
+    fontStyle: 'italic',
+    marginTop: 5,
+    alignSelf: 'center'
+  },
+  descriptionStl: {
+    color: '#000',
+    opacity: 0.54,
+    marginTop: 5,
+    alignSelf: 'center',
+    textAlign: 'justify'
+  }
+}
+
+export { styles }
